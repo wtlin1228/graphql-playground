9fb6ef0cc171558e773f0129c9b6174e4191eb79
wtlin1228 <wtlin1228@gmail.com>
2022-03-31 17:40:39 +0800

    feat: create graphqlRootModule

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,10 +1,29 @@
+import { join } from 'path';
+
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const graphqlRootModule = GraphQLModule.forRoot<ApolloDriverConfig>({
+  driver: ApolloDriver,
+
+  // To use Apollo Sandbox instead of the graphql-playground as a GraphQL IDE for local development.
+  playground: false,
+  plugins: [ApolloServerPluginLandingPageLocalDefault()],
+
+  // Generate TypeScript definitions (classes and interfaces) that correspond to the GraphQL SDL types.
+  typePaths: ['./**/*.graphql'],
+  definitions: {
+    path: join(process.cwd(), 'src/graphql.ts'),
+  },
+});
+
 @Module({
-  imports: [],
+  imports: [graphqlRootModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- docs/setup.md
+++ docs/setup.md
@@ -2,3 +2,5 @@
 2. `npm install --save-dev @nrwl/react`
 3. `npx nx g @nrwl/react:app client`
 4. `npx nx g @nrwl/nest:app server`
+5. `npm i @nestjs/graphql @nestjs/apollo graphql apollo-server-express`
+6. setup `graphqlRootModule` in `apps/server/src/app/app.module.ts`
